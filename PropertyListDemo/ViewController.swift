1442cf6172785fe7dfc733400818197903890064
mfouda214 <m.fouda@robone.net>
2018-04-07 17:37:50 +0100

    Print from Peaple plist

--- PropertyListDemo/ViewController.swift
+++ PropertyListDemo/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let plistUrl = Bundle.main.url(forResource: "People", withExtension: "plist")
+    let fileManager = FileManager.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var plistFormat = PropertyListSerialization.PropertyListFormat.xml
+        
+        do {
+            let plistData = try Data(contentsOf: plistUrl!)
+            let people = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as! [Dictionary<String, Any>]
+            
+            for person in people {
+                print("First Name: \(String(describing: person["First Name"])), Last Name: \(String(describing: person["Last Name"])), Age: \(String(describing: person["Age"]))")
+            }
+        } catch {
+            print("Error")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
