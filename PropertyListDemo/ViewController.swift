60e41d44b3a475bdf82921dfa53e07a4760a1d05
mfouda214 <m.fouda@robone.net>
2018-04-07 12:22:00 +0100

    initial commit

---
+++ PropertyListDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropertyListDemo
+//
+//  Created by Mohamed Sobhi  Fouda on 4/6/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
