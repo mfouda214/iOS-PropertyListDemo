4fcc59aa432d50bf1e1cd2bd24c11d916603363c
mfouda214 <m.fouda@robone.net>
2018-04-07 17:55:41 +0100

    move plist to Doc Directory

--- PropertyListDemo/ViewController.swift
+++ PropertyListDemo/ViewController.swift
@@ -17,23 +17,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var plistFormat = PropertyListSerialization.PropertyListFormat.xml
+//        var plistFormat = PropertyListSerialization.PropertyListFormat.xml
+//
+//        do {
+//            let plistData = try Data(contentsOf: plistUrl!)
+//            let people = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as! [Dictionary<String, Any>]
+//
+//            for person in people {
+//                print("First Name: \(String(describing: person["First Name"])), Last Name: \(String(describing: person["Last Name"])), Age: \(String(describing: person["Age"]))")
+//            }
+//
+//            // Unwrapped Example
+//            if let fName = people[0]["First Name"] {
+//                print("First Name for first person: \(fName)")
+//            }
+//        } catch {
+//            print("Error")
+//        }
         
-        do {
-            let plistData = try Data(contentsOf: plistUrl!)
-            let people = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as! [Dictionary<String, Any>]
-            
-            for person in people {
-                print("First Name: \(String(describing: person["First Name"])), Last Name: \(String(describing: person["Last Name"])), Age: \(String(describing: person["Age"]))")
-            }
-            
-            // Unwrapped Example
-            if let fName = people[0]["First Name"] {
-                print("First Name for first person: \(fName)")
-            }
-        } catch {
-            print("Error")
+        if fileExistsInDocumentsDirectory() == false {
+            seedDataToDocumentsDirectory()
         }
+
         
     }
 
@@ -42,6 +47,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func documentsDirectoryFileURL() -> URL? {
+        do {
+            let document = try fileManager.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+            let file = document.appendingPathComponent("People.plist")
+            return file
+        } catch {
+            print("Error getting file path.")
+            return nil
+        }
+    }
+    
+    func fileExistsInDocumentsDirectory() -> Bool {
+        if let file = documentsDirectoryFileURL() {
+            let fileExists = FileManager().fileExists(atPath: file.path)
+            return fileExists
+        }
+        
+        return false
+    }
+    
+    func seedDataToDocumentsDirectory() {
+        do {
+            let plistData = try Data(contentsOf: plistUrl!)
+            
+            if let file = documentsDirectoryFileURL() {
+                try plistData.write(to: file)
+            }
+        } catch {
+            print("Error writing file.")
+        }
+    }
 
 }
 
